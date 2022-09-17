024168870ed74412059b391333790d8a960e3fbc
Tianyu Liu <liu.tianyu93@hotmail.com>
2022-09-17 18:37:00 +0200

    Add light control

--- boardcomm.h
+++ boardcomm.h
@@ -11,6 +11,10 @@ typedef enum {
     GET_FAN_POWER = 1,
     SET_FAN_SPEED = 2 | 1 << 7,
     GET_FAN_SPEED = 2,
+    SET_LIGHT_POWER = 3 | 1 << 7,
+    GET_LIGHT_POWER = 3,
+    SET_LIGHT_BRIGHTNESS = 4 | 1 << 7,
+    GET_LIGHT_BRIGHTNESS = 4,
 
     UNKNOWN = 0,
 } CommandType;
@@ -21,7 +25,13 @@ typedef union {
 } FanControl;
 
 typedef union {
+    boolean enable;
+    uint8_t brightnessPercentage;
+} LightControl;
+
+typedef union {
     FanControl fanControl;
+    LightControl lightControl;
     uint8_t rawValue;
 } CommandValue;
 
