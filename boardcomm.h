1d801882b4894d37d903af20ff3666e87b4063c8
Tianyu Liu <liu.tianyu93@hotmail.com>
2022-10-17 18:11:40 +0200

    Update interface

--- boardcomm.h
+++ boardcomm.h
@@ -15,13 +15,17 @@ typedef enum {
     GET_LIGHT_POWER = 3,
     SET_LIGHT_BRIGHTNESS = 4 | 1 << 7,
     GET_LIGHT_BRIGHTNESS = 4,
+    SET_HEATER_POWER = 5 | 1 << 7,
+    GET_HEATER_POWER = 5,
+    SET_HEATER_OUTPUT = 6 | 1 << 7,
+    GET_HEATER_OUTPUT = 6,
 
     UNKNOWN = 0,
 } CommandType;
 
 typedef union {
     boolean enable;
-    uint8_t percentage;
+    uint8_t speedPercentage;
 } FanControl;
 
 typedef union {
@@ -30,8 +34,14 @@ typedef union {
 } LightControl;
 
 typedef union {
+    boolean enable;
+    uint8_t powerPercentage;
+} HeaterControl;
+
+typedef union {
     FanControl fanControl;
     LightControl lightControl;
+    HeaterControl heaterControl;
     uint8_t rawValue;
 } CommandValue;
 
